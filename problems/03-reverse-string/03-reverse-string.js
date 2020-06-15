39c165f1c75f52c5cba3246d3f26833519a3c5c4
RchlRiedel <rchlriedel27@gmail.com>
2020-06-15 11:18:14 -0400

    problem 3

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -3,4 +3,11 @@ Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
   
+    someStr.split("")
+    charArray = Array.from(someStr)
+    charArray.reverse();
+    return charArray.join("")
+
 }
+
+console.log(reverseStr("Hello World!"));
