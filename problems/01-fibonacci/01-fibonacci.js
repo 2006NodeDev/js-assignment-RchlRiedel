a5b1912589b4e3f7d0864c55448b9705da93e39f
RchlRiedel <rchlriedel27@gmail.com>
2020-06-15 16:08:59 -0400

    now improved with dynamic programming

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,20 +2,33 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 
-var n;
+//recursive function
+// function fib(n) {
 
-function fib(n) {
+//     if (n<=1)
+//         return n
+//     return fib(n-1) + fib(n-2)
 
-    if (n<=1)
-        return n
-    return fib(n-1) + fib(n-2)
-
-}
-
-//Display first 10 fibonacci numbers (not including 0)
-// for (var i = 1; i <=10; i++){
-//     console.log(fib(i));
 // }
 
-//Just display nth fibonacci number
-console.log(fib(23));
+//dynamic programming
+function fibImproved(n) {
+    let a = 0, b = 1, c; 
+        if (n == 0) 
+            return a; 
+        for (let i = 2; i <= n; i++) 
+        { 
+            c = a + b; 
+            a = b; 
+            b = c; 
+        } 
+        return b; 
+}
+//Just display a fibonacci number
+//console.log(fib(10));
+console.log(fibImproved(10)); 
+
+//Display first 10 fibonacci numbers (not including 0)
+// for (var i = 1; i <=23; i++){
+//     console.log(fibImproved(i));
+// }
