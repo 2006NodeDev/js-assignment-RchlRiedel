7b756dad75e561cedfc3c0793623efe43d2c9229
RchlRiedel <rchlriedel27@gmail.com>
2020-06-15 11:07:50 -0400

    problem 2

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -2,6 +2,14 @@
 Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
+
+//Sorts in ascending order
 function bubbleSort(numArray) {
-  
+
+   numArray.sort(function(a, b){return a-b})
+   return numArray
 }
+
+var anArray = [14, 35, 21, 49, 63, 56, 7, 28, 42, 70]
+
+console.log(bubbleSort(anArray))
