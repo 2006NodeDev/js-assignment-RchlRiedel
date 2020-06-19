46c51d1081aaad2d78f098cc6066484f255a087d
RchlRiedel <rchlriedel27@gmail.com>
2020-06-19 13:55:21 -0400

    problems 8 & 9

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,27 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
-
+    for (element in someObj) {
+        console.log(`${element}: ${someObj[element]}`);
+    }
+    console.log();
+    
 }
+
+function Pet (name, age, species, breed, coloring, cutenessLevel, characteristics) {
+    this.name = name
+    this.age = age
+    this.species = species
+    this.breed = breed
+    this.coloring = coloring
+    this.cutenessLevel = cutenessLevel
+    this.characteristics = characteristics
+}
+
+let Skittles = new Pet("Skittles", 8, "Cat","American Shorthair", "Grey splotches on white", 999, ["soft", "likes cheese", "part loaf"])
+let Sherman = new Pet("Sherman", 5, "Dog", "Great Dane", "Brown and white", -10, ["slow", "poops in neighbor's yard"])
+let Bobby = new Pet("Bobby", 7, "Rabbit", "American Fuzzy Lop", "Speckled brown on white", 300, ["fuzzy", "sassy", "easily intimidated"])
+
+objectProperties(Skittles)
+objectProperties(Sherman)
+objectProperties(Bobby)
--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,4 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
