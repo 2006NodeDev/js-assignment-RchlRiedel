d2b24e57a21e62a458396e884756e1c281185823
RchlRiedel <rchlriedel27@gmail.com>
2020-06-20 22:20:08 -0400

    final commit hopefully

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -2,3 +2,34 @@
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
 
+//object literal
+let bachelorContestant1 = {
+    name: "Karen",
+    age: 23,
+    occuptation: "Flight Attendant",
+    fakeTan: true
+}
+console.log(bachelorContestant1);
+
+//constructing function
+function bachelorContestant2(name, age, occupation, fakeTan){
+    this.name = name
+    this.age = age
+    this.occupation = occupation
+    this.fakeTan = fakeTan
+}
+let Amy = new bachelorContestant2("Amy", 25, "Waitress", false)
+console.log(Amy)
+
+//class
+class BachelorContestant3 {
+    constructor (name, age, occuptation, fakeTan){
+        this.name = name
+        this.age = age
+        this.occuptation = occuptation
+        this.fakeTan = fakeTan
+    }  
+}
+let Sydney = new BachelorContestant3("Sydney", 24, "Dancer", true)
+console.log(Sydney);
+
--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,13 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
+
+//I am assuming to "splice the element" is to get rid of that element
 function spliceElement(someArr, index) {
-  
+    someArr.splice(index-1, 1) //I use index -1 here to get rid of the nth term, not the term at index n
+    return someArr
 }
+
+//Get rid of roses
+let flowers = ["Violet", "Rose", "Lily", "Daisy"]
+console.log(spliceElement(flowers, 2));
