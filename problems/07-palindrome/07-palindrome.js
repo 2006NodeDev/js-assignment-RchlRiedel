d5ac716c8bba6eba489766fe3cfe60684551485b
RchlRiedel <rchlriedel27@gmail.com>
2020-06-20 22:26:32 -0400

    trimmed palindrome

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -3,11 +3,9 @@ Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
 
-    simpleStr = someStr.toUpperCase().replace(/\s+/g, ''); //to upper case, get rid of spaces with regular expression
-    simpleStr.split("") //split string
-    charArray = Array.from(simpleStr) //create array
-    charArray.reverse(); //reverse array
-    newStr = charArray.join("") //put array back into a string
+    simpleStr = someStr.toUpperCase().split(" ").join(""); //to upper case and to get rid of spaces
+    charArray = Array.from(simpleStr.split("")) //create array 
+    newStr = charArray.reverse().join("") //reverse array and put array back into a string
 
     // if (simpleStr === newStr) {
     //     return true
@@ -22,4 +20,5 @@ console.log(isPalindrome("Was it a car or a cat I saw"));
 console.log(isPalindrome("RaceCar"));
 
 console.log(isPalindrome("blahdaslkj"));
+
 
